2987e01c33fb6b6279dc6fe9dcc2186aff0f06ba
leadpencilusbcable <alexdel240@gmail.com>
2025-01-15 22:02:05 +1100

    initial commit

---
+++ tge.c
@@ -0,0 +1,343 @@
+#include <pthread.h>
+#include <signal.h>
+#include <stdatomic.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <sys/ioctl.h>
+#include <termios.h>
+#include <unistd.h>
+
+#include "tge.h"
+
+//TODO handle potential error codes from functions like tcsetattr
+//TODO I don't know if the current way of detecting keyboard input with atomics will work in actual situation
+//also it doesn't handle multiple keys concurrently
+
+static struct termios term_init_flags;
+static struct termios term_cur_flags;
+
+static tge_resize_callback resize_callback;
+
+static pthread_t keyboard_thread;
+
+unsigned short tge_rows;
+unsigned short tge_cols;
+
+unsigned short tge_cursor_x;
+unsigned short tge_cursor_y;
+
+static atomic_bool tge_keys_pressed[TGE_KEYS_SIZE];
+
+static void set_window_size(void){
+  struct winsize winsize;
+  ioctl(1, TIOCGWINSZ, &winsize);
+
+  tge_rows = winsize.ws_row;
+  tge_cols = winsize.ws_col;
+}
+
+static void handle_terminal_resize(int sig){
+  set_window_size();
+
+  if(resize_callback != NULL){
+    resize_callback(tge_rows, tge_cols);    
+  }
+}
+
+inline void tge_flush(void){
+  fflush(stdout);
+}
+
+inline void tge_clear(void){
+  fputs(TGE_CLEAR, stdout);
+}
+
+inline void tge_cursor_off(void){
+  fputs(TGE_CURSOR_OFF, stdout);
+}
+
+inline void tge_cursor_on(void){
+  fputs(TGE_CURSOR_ON, stdout);
+}
+
+void tge_echo_off(void){
+  term_cur_flags.c_lflag &= ~ECHO; 
+  tcsetattr(STDIN_FILENO, TCSANOW, &term_cur_flags);
+}
+
+void tge_echo_on(void){
+  term_cur_flags.c_lflag |= ECHO; 
+  tcsetattr(STDIN_FILENO, TCSANOW, &term_cur_flags);
+}
+
+void tge_canonical_mode_off(void){
+  term_cur_flags.c_lflag &= ~ICANON; 
+  tcsetattr(STDIN_FILENO, TCSANOW, &term_cur_flags);
+}
+
+void tge_canonical_mode_on(void){
+  term_cur_flags.c_lflag |= ICANON; 
+  tcsetattr(STDIN_FILENO, TCSANOW, &term_cur_flags);
+}
+
+void tge_raw_mode(void){
+  term_cur_flags.c_iflag &= ~(ICRNL | IXON);
+  term_cur_flags.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG); 
+  tcsetattr(STDIN_FILENO, TCSANOW, &term_cur_flags);
+}
+
+inline void tge_cursor_move_reset(void){
+  fputs(TGE_CURSOR_HOME, stdout);
+}
+
+bool tge_cursor_move_xy(unsigned short x, unsigned short y){
+  if(x >= tge_cols || y >= tge_rows){
+    return false;
+  }
+
+  printf("\x1B[%hi;%hiH", y, x);
+  tge_cursor_x = x;
+  tge_cursor_y = y;
+
+  return true;
+}
+
+bool tge_cursor_move_left(unsigned short n){
+  unsigned short new_x = tge_cursor_x - n;
+
+  if(new_x < 0){
+    return false;
+  }
+
+  printf("\x1B[%huD", n);
+  tge_cursor_x = new_x;
+
+  return true;
+}
+
+bool tge_cursor_move_right(unsigned short n){
+  unsigned short new_x = tge_cursor_x + n;
+
+  if(new_x >= tge_cols){
+    return false;
+  }
+
+  printf("\x1B[%huC", n);
+  tge_cursor_x = new_x;
+
+  return true;
+}
+
+bool tge_cursor_move_up(unsigned short n){
+  unsigned short new_y = tge_cursor_y - n;
+
+  if(new_y < 0){
+    return false;
+  }
+
+  printf("\x1B[%huA", n);
+  tge_cursor_y = new_y;
+
+  return true;
+}
+
+bool tge_cursor_move_down(unsigned short n){
+  unsigned short new_y = tge_cursor_y + n;
+
+  if(new_y >= tge_rows){
+    return false;
+  }
+
+  printf("\x1B[%huB", n);
+  tge_cursor_y = new_y;
+
+  return true;
+}
+
+void* update_keys(void* args){
+  while(true){
+    char c = getchar();
+
+    switch(c){
+      case 'a':
+      case 'A':
+        atomic_store_explicit(&tge_keys_pressed[TGE_KEY_A], true, memory_order_relaxed);
+        break;
+      case 'b':
+      case 'B':
+        atomic_store_explicit(&tge_keys_pressed[TGE_KEY_B], true, memory_order_relaxed);
+        break;
+      case 'c':
+      case 'C':
+        atomic_store_explicit(&tge_keys_pressed[TGE_KEY_C], true, memory_order_relaxed);
+        break;
+      case 'd':
+      case 'D':
+        atomic_store_explicit(&tge_keys_pressed[TGE_KEY_D], true, memory_order_relaxed);
+        break;
+      case 'e':
+      case 'E':
+        atomic_store_explicit(&tge_keys_pressed[TGE_KEY_E], true, memory_order_relaxed);
+        break;
+      case 'f':
+      case 'F':
+        atomic_store_explicit(&tge_keys_pressed[TGE_KEY_F], true, memory_order_relaxed);
+        break;
+      case 'g':
+      case 'G':
+        atomic_store_explicit(&tge_keys_pressed[TGE_KEY_G], true, memory_order_relaxed);
+        break;
+      case 'h':
+      case 'H':
+        atomic_store_explicit(&tge_keys_pressed[TGE_KEY_H], true, memory_order_relaxed);
+        break;
+      case 'i':
+      case 'I':
+        atomic_store_explicit(&tge_keys_pressed[TGE_KEY_I], true, memory_order_relaxed);
+        break;
+      case 'j':
+      case 'J':
+        atomic_store_explicit(&tge_keys_pressed[TGE_KEY_J], true, memory_order_relaxed);
+        break;
+      case 'k':
+      case 'K':
+        atomic_store_explicit(&tge_keys_pressed[TGE_KEY_K], true, memory_order_relaxed);
+        break;
+      case 'l':
+      case 'L':
+        atomic_store_explicit(&tge_keys_pressed[TGE_KEY_L], true, memory_order_relaxed);
+        break;
+      case 'm':
+      case 'M':
+        atomic_store_explicit(&tge_keys_pressed[TGE_KEY_M], true, memory_order_relaxed);
+        break;
+      case 'n':
+      case 'N':
+        atomic_store_explicit(&tge_keys_pressed[TGE_KEY_N], true, memory_order_relaxed);
+        break;
+      case 'o':
+      case 'O':
+        atomic_store_explicit(&tge_keys_pressed[TGE_KEY_O], true, memory_order_relaxed);
+        break;
+      case 'p':
+      case 'P':
+        atomic_store_explicit(&tge_keys_pressed[TGE_KEY_P], true, memory_order_relaxed);
+        break;
+      case 'q':
+      case 'Q':
+        atomic_store_explicit(&tge_keys_pressed[TGE_KEY_Q], true, memory_order_relaxed);
+        break;
+      case 'r':
+      case 'R':
+        atomic_store_explicit(&tge_keys_pressed[TGE_KEY_R], true, memory_order_relaxed);
+        break;
+      case 's':
+      case 'S':
+        atomic_store_explicit(&tge_keys_pressed[TGE_KEY_S], true, memory_order_relaxed);
+        break;
+      case 't':
+      case 'T':
+        atomic_store_explicit(&tge_keys_pressed[TGE_KEY_T], true, memory_order_relaxed);
+        break;
+      case 'u':
+      case 'U':
+        atomic_store_explicit(&tge_keys_pressed[TGE_KEY_U], true, memory_order_relaxed);
+        break;
+      case 'v':
+      case 'V':
+        atomic_store_explicit(&tge_keys_pressed[TGE_KEY_V], true, memory_order_relaxed);
+        break;
+      case 'w':
+      case 'W':
+        atomic_store_explicit(&tge_keys_pressed[TGE_KEY_W], true, memory_order_relaxed);
+        break;
+      case 'x':
+      case 'X':
+        atomic_store_explicit(&tge_keys_pressed[TGE_KEY_X], true, memory_order_relaxed);
+        break;
+      case 'y':
+      case 'Y':
+        atomic_store_explicit(&tge_keys_pressed[TGE_KEY_Y], true, memory_order_relaxed);
+        break;
+      case 'z':
+      case 'Z':
+        atomic_store_explicit(&tge_keys_pressed[TGE_KEY_Z], true, memory_order_relaxed);
+        break;
+    }
+  }
+
+  return NULL;
+}
+
+bool tge_key_pressed(unsigned int key_number){
+  return atomic_load_explicit(&tge_keys_pressed[key_number], memory_order_relaxed);
+}
+
+void tge_reset_all_keys(void){
+  for(size_t i = 0; i < TGE_KEYS_SIZE; i++){
+    atomic_store_explicit(&tge_keys_pressed[i], false, memory_order_relaxed);
+  }
+}
+
+void tge_init_key_events(void){
+  pthread_create(&keyboard_thread, NULL, &update_keys, NULL);
+}
+
+void tge_init_term_flags(void){
+  tcgetattr(STDIN_FILENO, &term_cur_flags);
+  term_init_flags = term_cur_flags;
+}
+
+void tge_init(void){
+  tge_init_term_flags();
+  tge_raw_mode();
+
+  tge_cursor_off();
+  tge_clear();
+  tge_cursor_move_reset();
+
+  tge_flush();
+
+  set_window_size();
+
+  struct sigaction sigact;
+  sigact.sa_handler = &handle_terminal_resize;
+  sigaction(SIGWINCH, &sigact, NULL);
+}
+
+void tge_clean(void){
+  tcsetattr(1, TCSANOW, &term_init_flags);
+  tge_cursor_on();
+  tge_flush();
+
+  if(keyboard_thread != 0){
+    pthread_cancel(keyboard_thread);
+  }
+}
+
+void tge_set_resize_callback(tge_resize_callback callback){
+  resize_callback = callback;
+}
+
+void tge_draw_game_object(struct tge_game_object game_object){
+  bool cursor_move_valid = tge_cursor_move_xy(game_object.pos.x, game_object.pos.y); 
+
+  unsigned short init_x_pos = tge_cursor_x;
+  char* itr = game_object.text;
+
+  while(*itr != '\0'){
+    if(!cursor_move_valid){
+      return;
+    }
+
+    if(*itr == '\n'){
+      cursor_move_valid = tge_cursor_move_xy(game_object.pos.x, tge_cursor_y + 1); 
+    } else {
+      putchar(*itr);
+    }
+
+    itr++;
+  }
+}
+
+
---
+++ tge.h
@@ -0,0 +1,120 @@
+#pragma once
+
+#include <stdatomic.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <termios.h>
+
+//TODO: atomics don't work in c++
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define TGE_CLEAR "\x1B[2J"
+#define TGE_CURSOR_OFF "\x1B[?25l"
+#define TGE_CURSOR_ON "\x1B[?25h"
+#define TGE_CURSOR_HOME "\x1B[H"
+
+#define TGE_KEY_A 0
+#define TGE_KEY_B 1
+#define TGE_KEY_C 2
+#define TGE_KEY_D 3
+#define TGE_KEY_E 4
+#define TGE_KEY_F 5
+#define TGE_KEY_G 6
+#define TGE_KEY_H 7
+#define TGE_KEY_I 8
+#define TGE_KEY_J 9
+#define TGE_KEY_K 10
+#define TGE_KEY_L 11
+#define TGE_KEY_M 12
+#define TGE_KEY_N 13
+#define TGE_KEY_O 14
+#define TGE_KEY_P 15
+#define TGE_KEY_Q 16
+#define TGE_KEY_R 17
+#define TGE_KEY_S 18
+#define TGE_KEY_T 19
+#define TGE_KEY_U 20
+#define TGE_KEY_V 21
+#define TGE_KEY_W 22
+#define TGE_KEY_X 23
+#define TGE_KEY_Y 24
+#define TGE_KEY_Z 25
+
+extern unsigned short tge_rows;
+extern unsigned short tge_cols;
+
+extern unsigned short tge_cursor_x;
+extern unsigned short tge_cursor_y;
+
+#define TGE_KEYS_SIZE 26
+
+/*Output all values written to stdout*/
+void tge_flush(void);
+/*Clear the terminal screen*/
+void tge_clear(void);
+/*Turn the terminal cursor off*/
+void tge_cursor_off(void);
+/*Turn the terminal cursor on*/
+void tge_cursor_on(void);
+/*Turn the echo off. Terminal will NOT display user's input*/
+void tge_echo_off(void);
+/*Turn the echo on. Terminal will display user's input*/
+void tge_echo_on(void);
+/*Turn canonical mode on. Terminal will buffer input as normal*/
+void tge_canonical_mode_off(void);
+/*Turn canonical mode off. Terminal will not buffer input until enter is pressed*/
+void tge_canonical_mode_on(void);
+/*Disable canonical mode, echo and a few other things.
+  This will also disable user commands that control the terminal, such as ctrl c to exit.*/
+void tge_raw_mode(void);
+/*Reset cursor to 0 0*/
+void tge_cursor_move_reset(void);
+/*Move the cursor to specified location*/
+bool tge_cursor_move_xy(unsigned short x, unsigned short y);
+/*Move cursor left n positions*/
+bool tge_cursor_move_left(unsigned short n);
+/*Move cursor right n positions*/
+bool tge_cursor_move_right(unsigned short n);
+/*Move cursor up n positions*/
+bool tge_cursor_move_up(unsigned short n);
+/*Move cursor down n positions*/
+bool tge_cursor_move_down(unsigned short n);
+
+struct tge_vec3 {
+  int x;  
+  int y;  
+  int z;  
+};
+
+struct tge_game_object {
+  struct tge_vec3 pos;
+  char* text;
+};
+
+/*Initialise thread that listens for key presses and sets them in tge_keys*/
+void tge_init_key_events(void);
+/*Retrieve initial terminal flags to be used in subsequent calls to tcsetattr*/
+void tge_init_term_flags(void);
+/*Initialise terminal. Can be done manually to customise behaviour.
+  Take a look at the source and copy the parts that suit your needs. */
+void tge_init(void);
+/*Cleans up terminal. Attempts to reset to state before running program*/
+void tge_clean(void);
+/*Check if a key has been pressed since last reset.
+  Keys in range 0-TGE_KEYS_SIZE. Macros defined for all keys, eg TGE_KEY_A*/
+bool tge_key_pressed(unsigned int key_number);
+/*Reset all keys to false*/
+void tge_reset_all_keys(void);
+
+typedef void (*tge_resize_callback) (unsigned short rows, unsigned short cols);
+/*Set a callback that is executed whenever terminal window is resized*/
+void tge_set_resize_callback(tge_resize_callback callback);
+/*Draw a game object to the screen*/
+void tge_draw_game_object(struct tge_game_object game_object);
+
+#ifdef __cplusplus
+}
+#endif
+
